87451ced1b1caea2eb0b6ca2f31428756f5173b2
ukeloveth <ukeloveth247@gmail.com>
2023-09-12 23:12:13 +0100

    Removed commented code

--- src/main/java/com/assessment/user/management/app/OAuthConfig/CustomOAuth2User.java
+++ src/main/java/com/assessment/user/management/app/OAuthConfig/CustomOAuth2User.java
@@ -32,8 +32,4 @@ public class CustomOAuth2User implements OAuth2User {
     public String getEmail() {
         return oauth2User.<String>getAttribute("email");
     }
-
-    public String getPicture() {
-        return oauth2User.<String>getAttribute("picture");
-    }
 }
